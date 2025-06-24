81a5679d3ac332b3eff2ab14dd155797ecc279bd
Chloe <lunamarychloeanne@gmail.com>
2025-06-24 18:16:37 +0800

    Adding contact section

---
+++ website_oop_version/contact_me.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def connect_section():
+    with st.container():
+        st.write("---")
+        st.subheader("Connect with me")
+        st.write("#####")
+        st.write("Email Account: lunamarychloeanne@gmail.com")
+        st.write("GitHub Account: [mrychlnn](https://github.com/mrychlnn)")
